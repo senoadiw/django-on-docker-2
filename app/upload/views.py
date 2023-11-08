e9f5eee19791a554ed236717f1475a799f50d194
Seno A. K <senoadiw@gmail.com>
2023-11-08 21:27:00 +0700

    update

--- app/upload/views.py
+++ app/upload/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from django.core.files.storage import FileSystemStorage
+from django.core.files.storage import FileSystemStorage, default_storage
+from django.conf import settings
 
 
 def image_upload(request):
     if request.method == "POST" and request.FILES["image_file"]:
         image_file = request.FILES["image_file"]
-        fs = FileSystemStorage()
-        filename = fs.save(image_file.name, image_file)
-        image_url = fs.url(filename)
+        storage = FileSystemStorage()
+        # use django-storages for production
+        if settings.DEBUG == 0:
+            storage = default_storage
+        filename = storage.save(image_file.name, image_file)
+        image_url = storage.url(filename)
         print(image_url)
-        return render(request, "upload.html", {
-            "image_url": image_url
-        })
+        return render(request, "upload.html", {"image_url": image_url})
     return render(request, "upload.html")
